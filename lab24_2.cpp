db604040a7437a1c46cd9cbffc1ed6cfdad8c9de
Jeng <tanabat2549kuayjeng@gmail.com>
2025-02-28 23:11:20 +0700

    cp

--- lab24_2.cpp
+++ lab24_2.cpp
@@ -15,7 +15,7 @@ void printList(list<string> s){
 int main(){
 	
 	list<string> line_up;
-	list<string>::iterator loc;
+	list<string>::iterator loc; 
 	
 	line_up.push_back("Alice");
 	line_up.push_back("Bob");
@@ -24,6 +24,25 @@ int main(){
 	line_up.insert(loc,"Oscar");
 	
 	//Write your code here
+	line_up.push_back("Luffy");
+	line_up.push_back("Sanji");
+	line_up.push_back("Nami");
+
+	line_up.pop_front();
+	line_up.pop_front();
+
+	loc = find(line_up.begin(),line_up.end(),"Sanji");
+	line_up.insert(loc,"Narutu");
+
+	line_up.push_front("Tony");
+	line_up.push_front("Prayath");
+
+	line_up.erase(next(line_up.begin(), 2));
+
+	line_up.pop_front();
+	line_up.pop_front();
+	line_up.pop_front();
+	
 	
 	printList(line_up);
 		
